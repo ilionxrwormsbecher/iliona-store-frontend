d43b5584c10d2f737ab6a84592743d3a261d13da
rwormsbecher@ilionx.com <Rodney22@live.nl>
2022-01-14 22:37:03 +0100

    set up styling and fix top header bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HeaderBar } from "./components/HeaderBar";
 import Nav from "./components/Nav";
 import PrivateRoute from "./helpers/PrivateRoute";
 import keycloak from "./Keycloak";
@@ -7,14 +8,16 @@ import keycloak from "./Keycloak";
 import WelcomePage from "./pages/Homepage";
 import SecuredPage from "./pages/SecuredPage";
 import { Unauthorized } from "./pages/Unauthorized";
+import "./styles/stylesheets/default.scss";
 
 function App() {
   return (
-    <div>
+    <div className="page-wrapper">
       <ReactKeycloakProvider
         authClient={keycloak}
         initOptions={{ onLoad: "login-required" }}
       >
+        <HeaderBar />
         <Nav />
         <BrowserRouter>
           <Routes>
---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const HeaderBar = () => {
+  return (
+    <div className="topbar-section">
+      <div className="brand">
+        <img src="/assets/img/logo.png" alt="ilionx logo" />
+      </div>
+
+      <div className="login-avatar"></div>
+    </div>
+  );
+};
