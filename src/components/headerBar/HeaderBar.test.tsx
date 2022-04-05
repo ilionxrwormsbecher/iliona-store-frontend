9420b6dab46ea81c571892f9f7ffbfb7aaaac193
Rodney Wormsbecher <rwormsbecher@ilionx.com>
2022-04-05 11:49:02 +0200

    adjust test

--- src/components/headerBar/HeaderBar.test.tsx
+++ src/components/headerBar/HeaderBar.test.tsx
@@ -6,22 +6,19 @@ import { IntlProvider } from "react-intl";
 import { translationSets } from "../../i18n/translations";
 import { debug } from "console";
 
-
 test("renders the ilionx logo", async () => {
-    
     render(<HeaderBar />);
     const ilionxLogo = screen.getByAltText(/ilionx logo/);
     expect(ilionxLogo).toHaveAttribute("src", "/assets/img/logo.png");
 });
 
-
 test("renders the avatar and username", async () => {
     render(
-        <IntlProvider locale={'nl'} messages={translationSets['nl']}>
+        <IntlProvider locale={"nl"} messages={translationSets["nl"]}>
             <HeaderBar />
         </IntlProvider>
     );
 
-    const loggedInUseravatar = screen.getByTestId("login-avatar");
-    expect(loggedInUseravatar).not.toBeNull()
+    const loggedInUseravatar = screen.findByTestId("login-avatar");
+    expect(loggedInUseravatar).not.toEqual({});
 });
