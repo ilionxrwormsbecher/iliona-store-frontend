7aa0c246363cd24c6a9dc1b010f2c4dd7e4b6802
rwormsbecher <rodney22@live.nl>
2022-06-01 11:19:08 +0200

    add startup.sh as linux lin endings

---
+++ startup.sh
@@ -0,0 +1,5 @@
+sed -i 's/|REACT_APP_API_URL|/$REACT_APP_API_URL/g' $(ls -d /usr/share/nginx/html/static/js/main.*?.js)
+sed -i 's/|REACT_APP_STOREFRONT_PORT|/$REACT_APP_STOREFRONT_PORT/g' $(ls -d /usr/share/nginx/html/static/js/main.*?.js)
+sed -i 's/|REACT_APP_BLOB_STORAGE_URL|/$REACT_APP_BLOB_STORAGE_URL/g' $(ls -d /usr/share/nginx/html/static/js/main.*?.js)
+
+nginx -g 'daemon off;'
