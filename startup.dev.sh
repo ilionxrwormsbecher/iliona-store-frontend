bacff7eefbe6389fcb8c9154618a20177673fa99
rwormsbecher <rodney22@live.nl>
2022-06-01 13:29:28 +0200

    remove startup.dev.sh

--- startup.dev.sh
+++
@@ -1,5 +0,0 @@
-sed -i 's/|REACT_APP_API_URL|/http:\/\/localhost:8001\//g' $(ls -d /usr/share/nginx/html/static/js/main.*?.js)
-sed -i 's/|REACT_APP_STOREFRONT_PORT|/10002/g' $(ls -d /usr/share/nginx/html/static/js/main.*?.js)
-sed -i 's/|REACT_APP_BLOB_STORAGE_URL|/https:\/\/ilionadev1001.blob.core.windows.net\/app-store-logos\//g' $(ls -d /usr/share/nginx/html/static/js/main.*?.js)
-
-nginx -g 'daemon off;'
